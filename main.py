2a303647ef955278525704d103eabf7944dd89f1
Arasy Bazwir <83108505+Aras1703@users.noreply.github.com>
2023-05-03 11:06:44 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
-import glints.glints as glints_auto
+from glints.glints import Glints
+from getpass4 import getpass
 import glints.constants as const
+import time
+
+# DO NOT ENABLE THE CREDENTIAL FUNCTION, IT MIGHT BE CAUSE AN ERROR WHILE AUTOMATING THE WEB
 
 try:
-    glints_auto.glints_test()
-    glints_auto.dropdown_languages('English')
-    #glints_auto.to_login_page()
-    glints_auto.to_jobs_page()
-    #glints_auto.to_explore_page()
-    glints_auto.search_jobs('QA Engineer')
-    glints_auto.job_types('internship', 'freelance')
-    glints_auto.remote_option()
-    glints_auto.work_locations(const.loc.get("Jkt"), const.loc.get("Bdg"))
-    glints_auto.block_advertises()
-    glints_auto.work_experiences(const.exp.get("<1"), const.exp.get("1-3"))
-    glints_auto.exclude_experience()
-
+    with Glints() as auto:
+        auto.glints_test()
+        auto.dropdown_languages('English')
+        auto.refresh()
+        auto.to_login_page(input("\nEmail or Phone number: "), getpass("\nPassword: "))
+        #auto.credential(input("\nEmail or Phone number: "), getpass("\nPassword: "))
+        auto.to_jobs_page()
+        auto.to_explore_page()
+        auto.search_jobs(input('\nWhat kind of job are you looking for ? '))
+        auto.job_filtration()
+        time.sleep(7)
+        auto.report_summary()
+        
 except Exception as e:
     if "in PATH" in str(e):
         print(
