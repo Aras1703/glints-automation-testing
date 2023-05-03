eb1ac2a18f7ab02c9e539db38a170679f4daa203
Arasy Bazwir <83108505+Aras1703@users.noreply.github.com>
2023-05-03 11:14:09 +0700

    Update constants.py

--- glints/constants.py
+++ glints/constants.py
@@ -1,8 +1,5 @@
 BASE_URL = "https://glints.com"
 languages = {"English":1, "Indonesia":2}
-credential = {"email":" ", # put your email
-              "phone":" ", # put your phone number
-              "password":" "} # put your password
 loc = {"Jkt":28904, "Tgr":27880, "Bdg":29367, "Sby":27918, "Jgj":27703}
 exp = {"<1":"LESS_THAN_A_YEAR", "1-3":"ONE_TO_THREE_YEARS", 
        "3-5":"THREE_TO_FIVE_YEARS", "5-10":"FIVE_TO_TEN_YEARS",
